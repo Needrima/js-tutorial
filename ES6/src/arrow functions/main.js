d678811a106fb3dfddfef653a6b88fed8cb4837d
Amirdeen Oyebode <oyebodeamirdeen@gmail.com>
2021-05-14 16:16:24 -0700

    commit

---
+++ ES6/src/arrow functions/main.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// normal function syntax
+let sum = function(a, b) {
+    console.log(a+b);
+}
+sum(5,19);
+
+// arrow function syntax
+let add = (a, b) => {
+    console.log(a+b);
+}
+add(16, 24);
+
